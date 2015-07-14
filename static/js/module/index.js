8685548e37625ea6f828e0b275ca7b537f4c61c6
IChocolateKapa <echochocolatekapa@163.com>
2015-07-14 17:24:33 +0800

    细致分析动画出现时间

--- static/js/module/index.js
+++ static/js/module/index.js
@@ -2,36 +2,16 @@
  * Created by hpwu on 2015/7/3.
  */
 
-var setTimeOutFunction = function(callback){
-    setTimeout(function(){callback}, 100);
-}
+
 
 $(function(){
-    setTimeOutFunction($("#_showHead").addClass("slide_rotate"));
-    setTimeOutFunction($("#_thirdHead").addClass("scale"));
-    setTimeOutFunction($("#_fourthHead").addClass("scale"));
-    setTimeOutFunction($("#_recommendHead").addClass("scale"));
-    var bodyHeight = $(window).height();
-    console.log(bodyHeight);
-    if(bodyHeight > 900){
-        setTimeOutFunction($("#_firstHead").addClass("scale"));
-        setTimeOutFunction($("#_secondHead").addClass("scale"));
-        setTimeOutFunction($("#_thirdHead").addClass("scale"));
-        setTimeOutFunction($("#_fourthHead").addClass("scale"));
-        setTimeOutFunction($("#_recommendHead").addClass("scale"));
-    }
+    var scrollTop = $(window).scrollTop();
+    setStatus(scrollTop);
 })
 
 
 $(window).on("scroll", function(){
     var scrollTop = $(window).scrollTop();
     console.log(scrollTop);
-    if(scrollTop >= 0 && scrollTop < 150){
-        setTimeOutFunction($("#_showHead").addClass("slide_rotate"));
-    } else if(scrollTop >= 150 && scrollTop <= 980){
-        setTimeOutFunction($("#_firstHead").addClass("scale"));
-        setTimeOutFunction($("#_secondHead").addClass("scale"));
-        setTimeOutFunction($("#_thirdHead").addClass("scale"));
-        setTimeOutFunction($("#_fourthHead").addClass("scale"));
-    }
+    setStatus(scrollTop);
 })
