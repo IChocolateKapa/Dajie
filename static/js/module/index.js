5bb42ead22548f23d9877c3ae0aa2d9d74e0882c
echo <echocholatekapa@163.com>
2015-07-19 19:07:28 +0800

    +侧边栏丰富事件

--- static/js/module/index.js
+++ static/js/module/index.js
@@ -7,6 +7,22 @@
 $(function(){
     var scrollTop = $(window).scrollTop();
     setStatus(scrollTop);
+
+    /*给侧边栏小图标 返回顶部 绑定事件 --开始*/
+    $("#goTop").bind("click", function(){
+        $(window).scrollTop(0);
+    })
+    /*给侧边栏小图标 返回顶部 绑定事件 --结束*/
+
+    /*给侧边栏小图标 意见反馈 绑定事件 --开始*/
+    $("#response").click(function(){
+        $(".floatBar").addClass("_showFloat");
+    })
+
+    $(".closeX").click(function(){
+        $(".floatBar").removeClass("_showFloat");
+    })
+    /*给侧边栏小图标 意见反馈 绑定事件 --结束*/
 })
 
 
